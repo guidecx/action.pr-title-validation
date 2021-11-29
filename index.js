a24e20dd9bf080da532e88e2eb738ec3aca45598
Steve McGee <solodesign31@gmail.com>
2021-11-29 15:12:32 -0700

    log test works, now try a PR title

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// import * as core from '@actions/core';
-// import * as github from '@actions/github';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 const prTitleRegex = new RegExp(
   '(chore|feat|feature|bug|fix|hotfix|revert)(([0-9]+)): [A-z 0-9]+',
@@ -8,19 +8,19 @@ const prTitleRegex = new RegExp(
 
 async function run() {
   console.log('running test...');
-  // try {
-  //   const title = github.context.payload.pull_request.title;
+  try {
+    const title = github.context.payload.pull_request.title;
 
-  //   const titleFailsCheck = prTitleRegex.test(title);
-  //   if (titleFailsCheck) {
-  //     throw Error(
-  //       '❌  Your PR title fails GCX standards, please refer to the company engineering standards for PR titles found at https://GCX-standards-doc-here/'
-  //     );
-  //   }
-  //   core.info('Thank you for the nice PR title!');
-  // } catch (error) {
-  //   core.info(error);
-  // }
+    const titleFailsCheck = prTitleRegex.test(title);
+    if (titleFailsCheck) {
+      throw Error(
+        '❌  Your PR title fails GCX standards, please refer to the company engineering standards for PR titles found at https://GCX-standards-doc-here/'
+      );
+    }
+    core.info('Thank you for the nice PR title!');
+  } catch (error) {
+    core.info(error);
+  }
 }
 
 run();
