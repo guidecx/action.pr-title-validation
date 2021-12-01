11cb928e46718fea72011f456f4fb6a0f9273d5e
Steve McGee <solodesign31@gmail.com>
2021-12-01 13:35:08 -0700

    working on adding WIP and draft PR

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-const prTitleRegex = /(chore|feat|feature|bug|fix|hotfix|revert)\([0-9]+\): [/' A-z 0-9]+/gim;
+const prTitleRegex = /(chore|feat|feature|bug|fix|hotfix|revert)\([0-9]+\): [/'\- A-z 0-9]+/gim;
 
-const passMessage = 'Thank you for the nice PR title!';
+const passMessage = '✅ Thank you for the nice PR title!';
 const failMessage =
-  '❌  Your PR title should look like `type(story-number): storyTitle or description` where `type` is one of [chore|feat|feature|bug|fix|hotfix|revert]';
+  '❌ Your PR title should resemble `type(story-number): short subject line` - see https://github.com/guidecx/action.pr-title-validation/blob/master/README.md';
 
 async function run() {
+  core.info(github.context.payload);
   const title = github.context.payload.pull_request.title;
   const titlePasses = prTitleRegex.test(title);
 
