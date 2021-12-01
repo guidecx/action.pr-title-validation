fe3cb9b49ec02f83890d1b17cd6ed0630f9f45fa
Steve McGee <solodesign31@gmail.com>
2021-12-01 14:39:28 -0700

    draft pr working

--- dist/index.js
+++ dist/index.js
@@ -8493,22 +8493,21 @@ __nccwpck_require__.r(__webpack_exports__);
 
 const prTitleRegex = /(chore|feat|feature|bug|fix|hotfix|revert)\([0-9]+\): [/'\- A-z 0-9]+/gim;
 
+const draftMessage = 'PR title not relevant on Draft PRs';
 const passMessage = '✅ Thank you for the nice PR title!';
 const failMessage =
   '❌ Your PR title should resemble `type(story-number): short subject line` - see https://github.com/guidecx/action.pr-title-validation/blob/master/README.md';
 
 async function run() {
-  console.log('draft:', JSON.stringify(_actions_github__WEBPACK_IMPORTED_MODULE_1__.context.payload.pull_request.draft, null, 2));
-  console.log('title:', JSON.stringify(_actions_github__WEBPACK_IMPORTED_MODULE_1__.context.payload.pull_request.title, null, 2));
   let titlePasses;
+
   const { title, draft } = _actions_github__WEBPACK_IMPORTED_MODULE_1__.context.payload.pull_request;
-  console.log('line 15 ~ draft:', draft);
-  console.log('line 15 ~ title:', title);
+
   titlePasses = draft ? true : prTitleRegex.test(title);
-  console.log('line 18 ~ titlePasses:', titlePasses);
 
   if (titlePasses) {
-    _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(passMessage);
+    _actions_core__WEBPACK_IMPORTED_MODULE_0__.notice(draft ? draftMessage : passMessage);
+    _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(draft ? draftMessage : passMessage);
   } else {
     _actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed(failMessage);
   }
--- index.js
+++ index.js
@@ -3,22 +3,21 @@ import * as github from '@actions/github';
 
 const prTitleRegex = /(chore|feat|feature|bug|fix|hotfix|revert)\([0-9]+\): [/'\- A-z 0-9]+/gim;
 
+const draftMessage = 'PR title not relevant on Draft PRs';
 const passMessage = '✅ Thank you for the nice PR title!';
 const failMessage =
   '❌ Your PR title should resemble `type(story-number): short subject line` - see https://github.com/guidecx/action.pr-title-validation/blob/master/README.md';
 
 async function run() {
-  console.log('draft:', JSON.stringify(github.context.payload.pull_request.draft, null, 2));
-  console.log('title:', JSON.stringify(github.context.payload.pull_request.title, null, 2));
   let titlePasses;
+
   const { title, draft } = github.context.payload.pull_request;
-  console.log('line 15 ~ draft:', draft);
-  console.log('line 15 ~ title:', title);
+
   titlePasses = draft ? true : prTitleRegex.test(title);
-  console.log('line 18 ~ titlePasses:', titlePasses);
 
   if (titlePasses) {
-    core.info(passMessage);
+    core.notice(draft ? draftMessage : passMessage);
+    core.info(draft ? draftMessage : passMessage);
   } else {
     core.setFailed(failMessage);
   }
